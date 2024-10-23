6c90e9045b0e1a5fc34d9381c4789e478b9fed17
Brieuc Desoutter <brieuc.desoutter@gmail.com>
2024-10-23 15:03:19 +0300

    fix(Backend): source .env.test for db password

--- Backend/Node/start_db.sh
+++ Backend/Node/start_db.sh
@@ -1,8 +1,10 @@
 #! /usr/bin/env sh
 
+. ./.env.test
+
 docker run -d \
     --name fleet-db \
-    -e POSTGRES_PASSWORD=mysecretpassword \
+    -e POSTGRES_PASSWORD=${DB_PWD} \
     -v ./db-data:/var/lib/postgresql/data \
     -p 5432:5432 \
     postgres
