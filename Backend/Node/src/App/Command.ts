50015f385272f459b18790a221749a24635f047b
Brieuc Desoutter <brieuc.desoutter@gmail.com>
2024-10-29 10:47:01 +0300

    refactor(Backend): create then load on constraint violation.

--- Backend/Node/src/App/Command.ts
+++ Backend/Node/src/App/Command.ts
@@ -3,17 +3,22 @@ import Location from "../Domain/Location.js";
 import { Vehicle, VehicleId } from "../Domain/Vehicle.js";
 import { Persistence } from "../Infra/Persistence.js";
 
+/**
+ * Create a new fleet.
+ *
+ * @param persistence {Persistence} the persistence context.
+ * @param userId {UserId} the user id of the fleet's owner
+ */
 export const createFleet = async (persistence: Persistence, userId: UserId): Promise<Fleet> => {
     return persistence.Fleets.create(userId);
 }
 
-// FIXME: we should get a transaction here
-const loadOrCreateVehicle = async (persistence: Persistence, vehicleId: VehicleId): Promise<Vehicle> => {
+const createOrLoadVehicle = async (persistence: Persistence, vehicleId: VehicleId): Promise<Vehicle> => {
     try {
-        const v = await persistence.Vehicles.load(vehicleId);
-        return v;
-    } catch (e) {
         const v = await persistence.Vehicles.create(vehicleId, 0.0, 0.0);
+        return v;
+    } catch (e) { // unique key constraint violation
+        const v = await persistence.Vehicles.load(vehicleId);
         return v;
     }
 }
@@ -23,7 +28,7 @@ export const registerVehicle = async (persistence: Persistence, fleetId: FleetId
     if (f == undefined) {
         throw new Error(`Fleet(id = ${fleetId} not found`);
     }
-    const v = await loadOrCreateVehicle(persistence, vehicleId);
+    const v = await createOrLoadVehicle(persistence, vehicleId);
     if (f.registerVehicle(v.id)) {
         await persistence.Fleets.save(f);
     }
